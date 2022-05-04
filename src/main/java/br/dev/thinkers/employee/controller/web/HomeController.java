63475d53eeebd628537e145f67c442bdd278efba
Glêsio Santos <61151196+glesiosantos@users.noreply.github.com>
2022-05-04 16:38:56 -0300

    create connect and setting flyway (#1)

---
+++ src/main/java/br/dev/thinkers/employee/controller/web/HomeController.java
@@ -0,0 +1,13 @@
+package br.dev.thinkers.employee.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home/index";
+    }
+}
