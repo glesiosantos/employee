8ab12cbfb3dfa5eb5d8ab4c5e9d2e9d5b111aa64
Glêsio Santos <glesioss@gmail.com>
2022-05-12 19:48:06 -0300

    create form employee

---
+++ src/main/java/br/dev/thinkers/employee/controller/web/EmployeeController.java
@@ -0,0 +1,15 @@
+package br.dev.thinkers.employee.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("employees")
+public class EmployeeController {
+
+    @GetMapping("/add")
+    public String addEmployee(){
+        return "employees/form";
+    }
+}
